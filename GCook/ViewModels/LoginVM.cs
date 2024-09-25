0a8e3df808b4d49f44141d34666dcdd6ff36dbd4
anglusa <otavio.baroni@etec.sp.gov.br>
2024-09-25 14:48:48 -0300

    Usuario service finalizado e inicio do RegistroVM

--- GCook/ViewModels/LoginVM.cs
+++ GCook/ViewModels/LoginVM.cs
@@ -7,10 +7,10 @@ public class LoginVM
     [Display(Name = "Email ou nome de usuário", Prompt = "Email ou nome de usuário")]
     [Required(ErrorMessage = "Por favor, informe seu email ou nome de usuário")]
     public string Email { get; set; }
-
+    
+    [Display(Name = "senha de acesso", Prompt = "********")]
+    [Required(ErrorMessage = "Por favor, informe sua senha")]
     [DataType(DataType.Password)]
-    [Display(Name = "senha de acesso", Prompt = "senha de acesso")]
-    [Required(ErrorMessage = "Por favor, informe sia senha")]
     public string Senha { get; set; }
 
     [Display(Name = "Manter conectado?")]          
--- GCook/ViewModels/RegistroVM.cs
+++ GCook/ViewModels/RegistroVM.cs
@@ -1,11 +1,37 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace GCook.ViewModels;
 
     public class RegistroVM
     {
-        
+        [Display(Name = "Nome Completo", Prompt ="Informe seu Nome Completo")]
+        [Required(ErrorMessage = "Por favor, informe seu Nome")]
+        [StringLength(60, ErrorMessage = "O Nome deve possuir no máximo 60 caracteres")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Data de Nascimento", Prompt ="Informe seu Data de Nascimento")]
+        [Required(ErrorMessage = "Por favor, informe sua Data de Nascimento")]
+        [DataType(DataType.Date)]
+        public DateTime? DataNascimento { get; set; } = null;
+
+        [Display(Name = "Nome Completo", Prompt ="Informe seu Nome Completo")]
+        [Required(ErrorMessage = "Por favor, informe seu Nome")]
+        [StringLength(60, ErrorMessage = "O Nome deve possuir no máximo 60 caracteres")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Nome Completo", Prompt ="Informe seu Nome Completo")]
+        [Required(ErrorMessage = "Por favor, informe seu Nome")]
+        [StringLength(60, ErrorMessage = "O Nome deve possuir no máximo 60 caracteres")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Nome Completo", Prompt ="Informe seu Nome Completo")]
+        [Required(ErrorMessage = "Por favor, informe seu Nome")]
+        [StringLength(60, ErrorMessage = "O Nome deve possuir no máximo 60 caracteres")]
+        public string Nome { get; set; }
+
+        public IFormFile Foto { get; set; }
+
+        public bool Termos { get; set; } = false;
+
+        public bool Enviado { get; set; } = false;
     }
